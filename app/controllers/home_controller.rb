5c3491b7a51699305ad4da476f51c669b52c8882
Camila Campos <camilacamposramos.si@gmail.com>
2015-10-01 00:58:36 -0300

    adding about and contact info

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
-  def index
-  end
+  def index; end
+
+  def about; end
+
+  def contact_info; end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'home#index'
 
+  get 'sobre' => 'home#about'
+  get 'contato' => 'home#contact_info'
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
