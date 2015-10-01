a9e7963a166aa5cda3076e9c904de1c9d60b0640
Camila Campos <camilacamposramos.si@gmail.com>
2015-10-01 01:00:13 -0300

    trying to make page pretty

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,8 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Enable the asset pipeline
+  config.assets.enabled = false
+  config.serve_static_files = false
 end
